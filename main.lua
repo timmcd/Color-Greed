fee0a5b92ccd4322f915d3b97e3b707a11ca4b1e
Timothy McDowell <tmcdowell@gmail.com>
2010-03-05 20:29:34 -0700

    love.callback

--- main.lua
+++ main.lua
@@ -1,24 +1,23 @@
 require("windowing/window.lua")
 
-function load()
+window = nil
+
+function love.load()
    -- Window object, with the new function. Takes a table with possibles being:
    -- -- color = { r, g, b, a }
    -- -- caption = "the caption as a string"
    -- -- position = { x, y }
    -- THE ARGUMENT loveLib IS REQUIRED!
-   args = { color = { 100, 0, 0, 0 }, caption = "Teh Window!", loveLib = }
+   args = { color = { 100, 0, 0, 0 }, caption = "Teh Window!", loveLib = love}
    window = Window:new(args)
    -- Set the position
    window.position = {5, 5}
-   -- Draw the window at it's current position
-   window:draw()
-   love.timer.sleep(5*1000)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
    -- Change the position and draw
    window:draw(50, 50)
-end
-
-function update(dt)
-end
-
-function draw()
 end
